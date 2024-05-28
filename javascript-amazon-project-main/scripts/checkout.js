9da5ff79ab48905baaac1fbea16bc4875270aa8b
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-05-28 22:41:59 +0545

    delete product from HTML using .remove method for DOM

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummaryHTML += `
-<div class="cart-item-container">
+<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -99,6 +99,8 @@ document.querySelectorAll(".js-delete-quantity-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId= link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    const toDelete = document.querySelector(`.js-cart-item-container-${productId}`);
+    toDelete.remove();
+
   });
 });
