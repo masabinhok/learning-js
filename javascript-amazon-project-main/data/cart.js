71937ed40ce59e7602068287a79369ef093a7e11
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-05-30 15:06:53 +0545

    make delivery options interactive

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,17 +1,17 @@
 export let cart = JSON.parse(localStorage.getItem("cart"));
 
 if (!cart) {
- cart = [
+  cart = [
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
-      deliveryOptionId:'1'
+      deliveryOptionId: "1",
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
-      deliveryOptionId:'2'
-    }
+      deliveryOptionId: "2",
+    },
   ];
 }
 
@@ -32,7 +32,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
-      deliveryOptionId:'1'
+      deliveryOptionId: "1",
     });
   }
   saveToStorage();
@@ -49,19 +49,31 @@ export function removeFromCart(productId) {
   saveToStorage();
 }
 
-export function calculateCartQuantity(){
+export function calculateCartQuantity() {
   let cartQuantity = 0;
   cart.forEach((cartItem) => {
-   cartQuantity += cartItem.quantity;
+    cartQuantity += cartItem.quantity;
   });
   return cartQuantity;
 }
 
-export function updateQuantity(productId, newQuantity){
-  cart.forEach((cartItem)=>{
-    if(cartItem.productId===productId){
-      cartItem.quantity=newQuantity;
+export function updateQuantity(productId, newQuantity) {
+  cart.forEach((cartItem) => {
+    if (cartItem.productId === productId) {
+      cartItem.quantity = newQuantity;
     }
-    localStorage.setItem('cart',JSON.stringify(cart));
-  })
+    localStorage.setItem("cart", JSON.stringify(cart));
+  });
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  saveToStorage();
 }
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -3,6 +3,7 @@ import {
   updateQuantity,
   cart,
   removeFromCart,
+  updateDeliveryOption,
 } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
@@ -110,7 +111,11 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += ` 
-        <div class="delivery-option">
+        <div class="delivery-option
+         js-delivery-option"
+          data-product-id="${
+          matchingProduct.id
+        }" data-delivery-option-id="${deliveryOption.id}">
         <input type="radio" ${isChecked ? "checked" : ""}
           class="delivery-option-input" 
           name="delivery-option-${matchingProduct.id}">
@@ -204,5 +209,12 @@ document.querySelectorAll(`.js-quantity-input`).forEach((link) => {
     if (event.key === "Enter") {
       console.log(event.key);
     }
+  });
+});
+
+document.querySelectorAll(".js-delivery-option").forEach((element) => {
+  element.addEventListener("click", () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
   });
 });
