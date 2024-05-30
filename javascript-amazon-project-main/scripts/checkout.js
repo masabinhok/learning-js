c4177fbbe7e6015afc00c72093c9d60325b8bcd2
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-05-30 14:57:14 +0545

    save data and generate html for delivery options

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -6,7 +6,11 @@ import {
 } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js"; //default export
+import { deliveryOptions } from "../data/deliveryOptions.js";
 
+//declarring an empty variable to store the generated HTML
 let cartSummaryHTML = "";
 
 cart.forEach((cartItem) => {
@@ -19,12 +23,26 @@ cart.forEach((cartItem) => {
     }
   });
 
+  //for deliverydate
+  const deliveryOptionId = cartItem.deliveryOptionId;
+
+  let deliveryOption;
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  const today = dayjs();
+  const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+  const dateString = deliveryDate.format("dddd, MMMM D");
+
   cartSummaryHTML += `
     <div class="cart-item-container js-cart-item-container-${
       matchingProduct.id
     }">
       <div class="delivery-date">
-        Delivery date: Wednesday, June 15
+        Delivery date:${dateString}
       </div>
 
       <div class="cart-item-details-grid">
@@ -71,51 +89,45 @@ cart.forEach((cartItem) => {
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-
-          <div class="delivery-option">
-            <input type="radio" class="delivery-option-input" name="delivery-option-${
-              matchingProduct.id
-            }">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio" checked class="delivery-option-input" name="delivery-option-${
-              matchingProduct.id
-            }">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio" class="delivery-option-input" name="delivery-option-${
-              matchingProduct.id
-            }">
-            <div>
-              <div class="delivery-option-date">
-                Monday, June 13
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
+            ${deliveryOptionsHTML(matchingProduct, cartItem)}
         </div>
       </div>
     </div>
   `;
 });
+
+function deliveryOptionsHTML(matchingProduct, cartItem) {
+  let html = "";
+  deliveryOptions.forEach((deliveryOption) => {
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+    const dateString = deliveryDate.format("dddd, MMMM D");
+    const priceString =
+      deliveryOption.priceCents === 0
+        ? `FREE`
+        : `$${formatCurrency(deliveryOption.priceCents)} -`;
+
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
+    html += ` 
+        <div class="delivery-option">
+        <input type="radio" ${isChecked ? "checked" : ""}
+          class="delivery-option-input" 
+          name="delivery-option-${matchingProduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            ${dateString}
+          </div>
+          <div class="delivery-option-price">
+            ${priceString} Shipping
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  return html;
+}
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 
@@ -131,6 +143,7 @@ document.querySelectorAll(".js-delete-quantity-link").forEach((link) => {
   });
 });
 
+// addign class for visibility
 document.querySelectorAll(".js-update-quantity-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
@@ -144,47 +157,52 @@ document.querySelectorAll(".js-update-quantity-link").forEach((link) => {
 
 updateCartQuantity();
 
+//updating cart quantity
+
 function updateCartQuantity() {
   const cartQuantity = calculateCartQuantity();
   document.querySelector(".js-return-to-home-link").innerHTML = cartQuantity;
 }
 
+hello(); //hellojs external library
 
+//dayjs library
 
+const today = dayjs();
+const deliveryDate = today.add(7, "days");
+console.log(deliveryDate.format("dddd, MMMM D"));
 
+//save button interactive
 
-  document.querySelectorAll(".js-save-quantity-link").forEach((link) => {
-    link.addEventListener("click", () => {
-      const productId = link.dataset.productId;
-      const container = document
-        .querySelectorAll(`.js-cart-item-container-${productId}`)
-        .forEach((item) => {
-          item.classList.remove("is-editing-quantity");
-        });
-    
-      document
-        .querySelectorAll(`.js-quantity-input-${productId}`)
-        .forEach((input) => {
-          const quantity = Number(input.value);
-          if (quantity >= 0 && quantity <= 1000) {
-            document.querySelector(`.js-quantity-label-${productId}`).innerHTML =
-              quantity;
-            updateQuantity(productId, quantity);
-            updateCartQuantity();
-          } else alert("Items cannot be negative");
-        });
-    });
+document.querySelectorAll(".js-save-quantity-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    const container = document
+      .querySelectorAll(`.js-cart-item-container-${productId}`)
+      .forEach((item) => {
+        item.classList.remove("is-editing-quantity");
+      });
+
+    document
+      .querySelectorAll(`.js-quantity-input-${productId}`)
+      .forEach((input) => {
+        const quantity = Number(input.value);
+        if (quantity >= 0 && quantity <= 1000) {
+          document.querySelector(`.js-quantity-label-${productId}`).innerHTML =
+            quantity;
+          updateQuantity(productId, quantity);
+          updateCartQuantity();
+        } else alert("Items cannot be negative");
+      });
   });
-  
+});
 
+// failed to deploy enter key shortcut
 
 document.querySelectorAll(`.js-quantity-input`).forEach((link) => {
   link.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
-    console.log(event.key);
-
+      console.log(event.key);
     }
   });
 });
-
-
